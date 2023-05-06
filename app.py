8c14dbbe1f063b3a98c8f186653535c3c69f6406
jvill171 <juanvillarreal64@yahoo.com>
2023-05-06 16:28:16 -0500

    Added some routes to use; Still not fully functional

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import os
 from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
-from my_secrets import MY_SECRET_KEY, DB_NAME
+from forms import UserAddForm, LoginForm, EditUserForm
+from models import User, Game
 
+from my_secrets import MY_SECRET_KEY, DB_NAME
+from pkmn_list import pokemon_list
 
 # from forms import 
 from models import db, connect_db
@@ -24,6 +27,70 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
+# ******************** BEFORE_REQUEST ********************
+@app.before_request
+def add_user_to_g():
+    """If logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+# ******************** ROUTES ********************
 @app.route("/")
 def homepage():
-    return "Hello World"
+    return render_template('base.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    
+    if form.validate_on_submit():
+        # Login stuff
+        print('logged in!')
+
+    return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    
+    # Do logout stuff
+    return redirect('/')
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+
+    form=UserAddForm()
+    form.fav_pkmn.choices = [(idx, f"{idx} - {pkmn}") for (idx, pkmn) in enumerate(['None'] + pokemon_list[:])]
+
+    if form.validate_on_submit():
+        # Register stuff
+        print("Registered!" )
+        
+    return render_template('signup.html', form=form)
+
+
+@app.route("/profile/<int:user_id>", methods=['GET', 'POST'])
+def user_profile(user_id):
+    form=EditUserForm()
+    return render_template('profile.html', form=form)
+
+@app.route("/leaderboard")
+def leaderboard():
+
+    return render_template('leaderboard.html')
