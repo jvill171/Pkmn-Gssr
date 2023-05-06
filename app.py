864689883c2c8a5e7245632203a7c3bd5e51be7f
jvill171 <juanvillarreal64@yahoo.com>
2023-05-06 18:29:15 -0500

    Added logic to register a user & some descriptions for routes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
-
+import requests
 from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 from forms import UserAddForm, LoginForm, EditUserForm
 from models import User, Game
+from random import randint
 
 from my_secrets import MY_SECRET_KEY, DB_NAME
 from pkmn_list import pokemon_list
@@ -13,8 +14,10 @@ from pkmn_list import pokemon_list
 from models import db, connect_db
 
 CURR_USER_KEY = "curr_user"
+BASE_API_URL = "https://pokeapi.co/api/v2/"
 
 app = Flask(__name__)
+app.app_context().push()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', DB_NAME))
@@ -26,6 +29,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', MY_SECRET_KEY)
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+# db.create_all()
 
 # ******************** BEFORE_REQUEST ********************
 @app.before_request
@@ -51,11 +55,13 @@ def do_logout():
 # ******************** ROUTES ********************
 @app.route("/")
 def homepage():
+    """Homepage - Games take place here"""
     return render_template('base.html')
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    """Handle a user attempting to log in"""
     form=LoginForm()
     
     if form.validate_on_submit():
@@ -67,20 +73,54 @@ def login():
 
 @app.route("/logout")
 def logout():
+    """Handle a user logging out"""
     
     # Do logout stuff
+    do_logout()
+    flash("Logged out!", "success")
     return redirect('/')
 
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    """Handle a user signing up. Also determines which pokemon is the user's favorite pokemon"""
 
     form=UserAddForm()
     form.fav_pkmn.choices = [(idx, f"{idx} - {pkmn}") for (idx, pkmn) in enumerate(['None'] + pokemon_list[:])]
 
     if form.validate_on_submit():
-        # Register stuff
-        print("Registered!" )
+        dex_number = int(form.fav_pkmn.data)
+
+        # Get image URL to send to User.signup
+        if(dex_number == 0):
+            image_url = None
+        else:
+            try:
+                resp = requests.get(f"{BASE_API_URL}/pokemon/{dex_number}")
+                img_data = resp.json()['sprites']['other']['official-artwork']
+                image_url = img_data['front_shiny'] if (randint(1, 512) == 512) else img_data['front_default']
+            except:
+                image_url = None
+                flash("An error occursed with your favorite pokemon. Default image set.", "danger")
+        
+        # Sign up the user using signup classmethod
+        try:    
+            user = User.signup(
+                username = form.username.data, 
+                password = form.password.data,
+                email    = form.email.data,
+                favorite = pokemon_list[dex_number] if 0 < dex_number <= len(pokemon_list) else "No favorite pokemon",
+                img_url  = image_url
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", "danger")
+            return render_template('signup.html', form=form)
+        
+        # Log in & redirect to homepage
+        do_login(user)
+        return redirect("/")
         
     return render_template('signup.html', form=form)
 
@@ -92,5 +132,6 @@ def user_profile(user_id):
 
 @app.route("/leaderboard")
 def leaderboard():
+    """Display leaderboards for all players"""
 
     return render_template('leaderboard.html')
