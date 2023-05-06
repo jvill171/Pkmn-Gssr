99eeea816f15377d2be856fd6e3365622f58e8dc
jvill171 <juanvillarreal64@yahoo.com>
2023-05-06 11:51:06 -0500

    Added initial test route

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask, render_template, request, flash, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+from my_secrets import MY_SECRET_KEY
+
+
+# from forms import 
+from models import db, connect_db
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///pkmn_gssr'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', MY_SECRET_KEY)
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route("/")
+def homepage():
+    return "Hello World"
