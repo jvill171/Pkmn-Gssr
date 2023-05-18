a3fa92899438191fc588513569aa8bb2ee434764
jvill171 <juanvillarreal64@yahoo.com>
2023-05-17 22:23:03 -0500

    Added time attack game mode & logic

---
+++ static/timer_script.js
@@ -0,0 +1,38 @@
+const $timer = $(".game_timer")
+let timerCountdown
+let paused = true
+
+function startTimer(){
+    resumeTimer()
+    timerCountdown = setInterval(()=>{
+        if(!paused){
+            console.log("Paused = ", paused)
+            $timer.text(Number($timer.text())-1)
+            
+            if($timer.text() <= 0){
+                $("#guess-btn").text("Time's Up!")
+                $timer.text(0)
+                clearInterval(timerCountdown)
+
+                let finalGuess = $(".all-guesses").children().last().children().first().text()
+                endAnswer(finalGuess)
+            }
+        }
+    }, 1000)
+}
+
+function stopTimer(){
+    clearInterval(timerCountdown)
+}
+function pauseTimer(){
+    paused = true
+}
+function resumeTimer(){
+    paused = false
+}
+
+function resetTimer(){
+    clearInterval(timerCountdown)
+    $("#guess-btn").prop("disabled",false).text("Submit")
+    $timer.text(120)
+}
